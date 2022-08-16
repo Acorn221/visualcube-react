78aeed69517de130af4b1d9ed497383c9204c609
James Arnott <jamesarnott2@gmail.com>
2022-08-16 12:54:43 +0100

    fix: ICubeOptionsComplete now has appropriate required fields

--- visualcube-react/src/cube/options.ts
+++ visualcube-react/src/cube/options.ts
@@ -35,27 +35,18 @@ export interface ICubeOptions {
 }
 
 export interface ICubeOptionsComplete {
-  dist: number
-  algorithm: string
-  case: string
-  backgroundColor: string
-  cubeColor: string
-  outlineWidth: number
-  strokeWidth: number
   cubeSize: number
-  cubeOpacity: number
-  stickerOpacity: number
-  colorScheme: { [face: number]: string }
-  maskColor: string
-  stickerColors: string[]
-  facelets: string[] | FaceletDefinition[]
-  viewportRotations: [Axis, number][]
-  view: string
   width: number
   height: number
-  mask: Masking
-  maskAlg: string
-  arrows: Arrow[] | string
+  viewportRotations: [Axis, number][]
+  colorScheme: { [face: number]: string }
+  cubeColor: string
+  cubeOpacity: number
+  stickerOpacity: number
+  dist: number
+  outlineWidth: number
+  strokeWidth: number
+
   viewbox: {
     // SVG viewbox settings
     x: number
@@ -63,4 +54,16 @@ export interface ICubeOptionsComplete {
     width: number
     height: number
   }
+
+  algorithm?: string
+  case?: string
+  backgroundColor?: string  
+  maskColor?: string
+  stickerColors?: string[]
+  facelets?: FaceletDefinition[]
+  view?: string
+  mask?: Masking
+  maskAlg?: string
+  arrows?: Arrow[] | string
+  
 }
