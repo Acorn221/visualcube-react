26453ab61ff86ce4879541a08c0a434e9efbb969
James <jamesarnott2@gmail.com>
2022-08-30 18:27:07 +0100

    lint: manual linting

--- src/cube/drawing.ts
+++ src/cube/drawing.ts
@@ -29,89 +29,11 @@ const defaultFaceRotations: FaceRotations = {
   [Face.B]: [0, 0, 1],
 };
 
-export const renderCube = (
-  container: HTMLElement | string,
-  geometry: CubeGeometry,
-  options: ICubeOptionsComplete,
-) => {
-  const faceRotations = rotateFaces(
-    defaultFaceRotations,
-    options.viewportRotations,
-  );
-  const renderOrder = getRenderOrder(faceRotations);
-  const svg = SVG(container as HTMLElement).size(options.width, options.height);
-  svg.viewbox(
-    options.viewbox.x,
-    options.viewbox.y,
-    options.viewbox.width,
-    options.viewbox.height,
-  );
-
-  const hiddenFaces = renderOrder.filter(
-    (face) => !faceVisible(face, faceRotations),
-  );
-  const visibleFaces = renderOrder.filter((face) => faceVisible(face, faceRotations));
-
-  let cubeOutlineGroup: SVG.G;
-
-  renderBackground(svg, options);
-  // Render hidden faces if cube color has transparency
-  if (options.cubeOpacity < 100) {
-    cubeOutlineGroup = getCubeOutlineGroup(svg, options);
-    hiddenFaces.forEach((face) => {
-      renderFaceStickers(svg, face, geometry[face], options);
-      renderCubeOutline(cubeOutlineGroup, geometry[face], options);
-    });
-  }
-
-  cubeOutlineGroup = getCubeOutlineGroup(svg, options);
-  visibleFaces.forEach((face) => {
-    renderCubeOutline(cubeOutlineGroup, geometry[face], options);
-    renderFaceStickers(svg, face, geometry[face], options);
-  });
-
-  if (options.view === 'plan') {
-    const ollGroup = getOllLayerGroup(svg, options);
-    [Face.R, Face.F, Face.L, Face.B].forEach((face) => {
-      renderOLLStickers(ollGroup, face, geometry[face], faceRotations, options);
-    });
-  }
-
-  const arrowGroup = getArrowGroup(svg, geometry[0].length - 1);
-  let arrowDefinitions: Arrow[] = [];
-
-  if (Array.isArray(options.arrows)) {
-    arrowDefinitions = options.arrows;
-  } else if (typeof options.arrows === 'string') {
-    arrowDefinitions = parseArrows(options.arrows);
-  }
-
-  arrowDefinitions.forEach((arrow) => {
-    renderArrow(arrowGroup, geometry, arrow);
-  });
-
-  return svg;
-};
-
 /**
  * Determines face render order based on z position. Faces further away
  * will render first so anything closer will be drawn on top.
  */
 const getRenderOrder = (faceRotations: FaceRotations): Face[] => [...AllFaces].sort((a: Face, b: Face) => faceRotations[b][2] - faceRotations[a][2]);
-
-const renderBackground = (svg: SVG.Doc, options: ICubeOptionsComplete) => {
-  const backgroundSvg = svg.rect(options.viewbox.width, options.viewbox.height);
-  backgroundSvg.x(options.viewbox.x);
-  backgroundSvg.y(options.viewbox.y);
-  if (!options.backgroundColor) {
-    backgroundSvg.fill('none');
-    backgroundSvg.opacity(0);
-  } else {
-    backgroundSvg.fill({
-      color: options.backgroundColor,
-    });
-  }
-};
 
 function faceVisible(face: Face, rotations: FaceRotations) {
   return rotations[face][2] < -0.105;
@@ -141,16 +63,19 @@ function getOllLayerGroup(svg: SVG.Doc, options: ICubeOptionsComplete): SVG.G {
   return group;
 }
 
-function getArrowGroup(svg: SVG.Doc, cubeSize: number): SVG.G {
-  const arrowGroup = svg.group();
-  arrowGroup.attr({
-    opacity: 1,
-    'stroke-opacity': 1,
-    'stroke-width': 0.12 / cubeSize,
-    'stroke-linecap': 'round',
-  });
-  return arrowGroup;
-}
+const renderBackground = (svg: SVG.Doc, options: ICubeOptionsComplete) => {
+  const backgroundSvg = svg.rect(options.viewbox.width, options.viewbox.height);
+  backgroundSvg.x(options.viewbox.x);
+  backgroundSvg.y(options.viewbox.y);
+  if (!options.backgroundColor) {
+    backgroundSvg.fill('none');
+    backgroundSvg.opacity(0);
+  } else {
+    backgroundSvg.fill({
+      color: options.backgroundColor,
+    });
+  }
+};
 
 function renderCubeOutline(
   svg: SVG.G,
@@ -168,66 +93,6 @@ function renderCubeOutline(
   const polygon = svg.polygon(outlinePoints);
   polygon.fill(options.cubeColor);
   polygon.stroke(options.cubeColor);
-  return polygon;
-}
-
-function renderFaceStickers(
-  svg: SVG.Doc,
-  face: Face,
-  stickers: FaceStickers,
-  options: ICubeOptionsComplete,
-): SVG.G {
-  const cubeSize = stickers.length - 1;
-  const group = svg.group();
-  group.opacity(options.stickerOpacity / 100);
-  group.attr({
-    'stoke-opacity': '0.5',
-    'stroke-width': options.strokeWidth,
-    'stroke-linejoin': 'round',
-  });
-
-  for (let i = 0; i < cubeSize; i++) {
-    for (let j = 0; j < cubeSize; j++) {
-      const centerPoint: Vec3 = [
-        (stickers[j][i][0] + stickers[j + 1][i + 1][0]) / 2,
-        (stickers[j][i][1] + stickers[j + 1][i + 1][1]) / 2,
-        0,
-      ];
-
-      // Scale points in towards centre
-      const p1 = transScale(stickers[j][i], centerPoint, 0.85);
-      const p2 = transScale(stickers[j + 1][i], centerPoint, 0.85);
-      const p3 = transScale(stickers[j + 1][i + 1], centerPoint, 0.85);
-      const p4 = transScale(stickers[j][i + 1], centerPoint, 0.85);
-
-      const color = getStickerColor(face, i, j, options);
-      if (color !== ColorName.Transparent) {
-        renderSticker(group, p1, p2, p3, p4, color, options.cubeColor);
-      }
-    }
-  }
-
-  return group;
-}
-
-function renderSticker(
-  g: SVG.G,
-  p1: Vec3,
-  p2: Vec3,
-  p3: Vec3,
-  p4: Vec3,
-  stickerColor: string,
-  cubeColor: string,
-): SVG.Polygon {
-  const stickerPoints = [
-    [p1[0], p1[1]],
-    [p2[0], p2[1]],
-    [p3[0], p3[1]],
-    [p4[0], p4[1]],
-  ];
-  const polygon = g.polygon(stickerPoints);
-  polygon.fill(stickerColor);
-  polygon.stroke(cubeColor);
   return polygon;
 }
 
@@ -286,35 +151,64 @@ function getStickerColor(
   return options.colorScheme[face] || ColorName.Black;
 }
 
-// Renders the top rim of the R U L and B faces out from side of cube
-export function renderOLLStickers(
-  group: SVG.G,
+function renderSticker(
+  g: SVG.G,
+  p1: Vec3,
+  p2: Vec3,
+  p3: Vec3,
+  p4: Vec3,
+  stickerColor: string,
+  cubeColor: string,
+): SVG.Polygon {
+  const stickerPoints = [
+    [p1[0], p1[1]],
+    [p2[0], p2[1]],
+    [p3[0], p3[1]],
+    [p4[0], p4[1]],
+  ];
+  const polygon = g.polygon(stickerPoints);
+  polygon.fill(stickerColor);
+  polygon.stroke(cubeColor);
+  return polygon;
+}
+
+function renderFaceStickers(
+  svg: SVG.Doc,
   face: Face,
   stickers: FaceStickers,
-  rotations: FaceRotations,
   options: ICubeOptionsComplete,
-) {
-  // Translation vector, to move faces out
-  const v1 = scale(rotations[face], 0);
-  const v2 = scale(rotations[face], 0.2);
-  for (let i = 0; i < options.cubeSize; i++) {
-    // find center point of sticker
-    const centerPoint: Vec3 = [
-      (stickers[i][0][0] + stickers[i + 1][1][0]) / 2,
-      (stickers[i][0][1] + stickers[i + 1][1][1]) / 2,
-      0,
-    ];
-    const p1 = translate(transScale(stickers[i][0], centerPoint, 0.94), v1);
-    const p2 = translate(transScale(stickers[i + 1][0], centerPoint, 0.94), v1);
-    const p3 = translate(transScale(stickers[i + 1][1], centerPoint, 0.94), v2);
-    const p4 = translate(transScale(stickers[i][1], centerPoint, 0.94), v2);
+): SVG.G {
+  const cubeSize = stickers.length - 1;
+  const group = svg.group();
+  group.opacity(options.stickerOpacity / 100);
+  group.attr({
+    'stoke-opacity': '0.5',
+    'stroke-width': options.strokeWidth,
+    'stroke-linejoin': 'round',
+  });
 
-    const stickerColor = getStickerColor(face, 0, i, options);
+  for (let i = 0; i < cubeSize; i++) {
+    for (let j = 0; j < cubeSize; j++) {
+      const centerPoint: Vec3 = [
+        (stickers[j][i][0] + stickers[j + 1][i + 1][0]) / 2,
+        (stickers[j][i][1] + stickers[j + 1][i + 1][1]) / 2,
+        0,
+      ];
 
-    if (stickerColor !== ColorName.Transparent) {
-      renderSticker(group, p1, p2, p3, p4, stickerColor, options.cubeColor);
+      // Scale points in towards centre
+      const p1 = transScale(stickers[j][i], centerPoint, 0.85);
+      const p2 = transScale(stickers[j + 1][i], centerPoint, 0.85);
+      const p3 = transScale(stickers[j + 1][i + 1], centerPoint, 0.85);
+      const p4 = transScale(stickers[j][i + 1], centerPoint, 0.85);
+
+      const color = getStickerColor(face, i, j, options);
+      if (color !== ColorName.Transparent) {
+        renderSticker(group, p1, p2, p3, p4, color, options.cubeColor);
+      }
     }
   }
+
+  return group;
 }
 
 /**
@@ -411,3 +305,109 @@ export function renderArrow(
     'stroke-linejoin': 'round',
   });
 }
+
+function getArrowGroup(svg: SVG.Doc, cubeSize: number): SVG.G {
+  const arrowGroup = svg.group();
+  arrowGroup.attr({
+    opacity: 1,
+    'stroke-opacity': 1,
+    'stroke-width': 0.12 / cubeSize,
+    'stroke-linecap': 'round',
+  });
+  return arrowGroup;
+}
+
+// Renders the top rim of the R U L and B faces out from side of cube
+export function renderOLLStickers(
+  group: SVG.G,
+  face: Face,
+  stickers: FaceStickers,
+  rotations: FaceRotations,
+  options: ICubeOptionsComplete,
+) {
+  // Translation vector, to move faces out
+  const v1 = scale(rotations[face], 0);
+  const v2 = scale(rotations[face], 0.2);
+  for (let i = 0; i < options.cubeSize; i++) {
+    // find center point of sticker
+    const centerPoint: Vec3 = [
+      (stickers[i][0][0] + stickers[i + 1][1][0]) / 2,
+      (stickers[i][0][1] + stickers[i + 1][1][1]) / 2,
+      0,
+    ];
+    const p1 = translate(transScale(stickers[i][0], centerPoint, 0.94), v1);
+    const p2 = translate(transScale(stickers[i + 1][0], centerPoint, 0.94), v1);
+    const p3 = translate(transScale(stickers[i + 1][1], centerPoint, 0.94), v2);
+    const p4 = translate(transScale(stickers[i][1], centerPoint, 0.94), v2);
+
+    const stickerColor = getStickerColor(face, 0, i, options);
+
+    if (stickerColor !== ColorName.Transparent) {
+      renderSticker(group, p1, p2, p3, p4, stickerColor, options.cubeColor);
+    }
+  }
+}
+
+export const renderCube = (
+  container: HTMLElement | string,
+  geometry: CubeGeometry,
+  options: ICubeOptionsComplete,
+) => {
+  const faceRotations = rotateFaces(
+    defaultFaceRotations,
+    options.viewportRotations,
+  );
+  const renderOrder = getRenderOrder(faceRotations);
+  const svg = SVG(container as HTMLElement).size(options.width, options.height);
+  svg.viewbox(
+    options.viewbox.x,
+    options.viewbox.y,
+    options.viewbox.width,
+    options.viewbox.height,
+  );
+
+  const hiddenFaces = renderOrder.filter(
+    (face) => !faceVisible(face, faceRotations),
+  );
+  const visibleFaces = renderOrder.filter((face) => faceVisible(face, faceRotations));
+
+  let cubeOutlineGroup: SVG.G;
+
+  renderBackground(svg, options);
+  // Render hidden faces if cube color has transparency
+  if (options.cubeOpacity < 100) {
+    cubeOutlineGroup = getCubeOutlineGroup(svg, options);
+    hiddenFaces.forEach((face) => {
+      renderFaceStickers(svg, face, geometry[face], options);
+      renderCubeOutline(cubeOutlineGroup, geometry[face], options);
+    });
+  }
+
+  cubeOutlineGroup = getCubeOutlineGroup(svg, options);
+  visibleFaces.forEach((face) => {
+    renderCubeOutline(cubeOutlineGroup, geometry[face], options);
+    renderFaceStickers(svg, face, geometry[face], options);
+  });
+
+  if (options.view === 'plan') {
+    const ollGroup = getOllLayerGroup(svg, options);
+    [Face.R, Face.F, Face.L, Face.B].forEach((face) => {
+      renderOLLStickers(ollGroup, face, geometry[face], faceRotations, options);
+    });
+  }
+
+  const arrowGroup = getArrowGroup(svg, geometry[0].length - 1);
+  let arrowDefinitions: Arrow[] = [];
+
+  if (Array.isArray(options.arrows)) {
+    arrowDefinitions = options.arrows;
+  } else if (typeof options.arrows === 'string') {
+    arrowDefinitions = parseArrows(options.arrows);
+  }
+
+  arrowDefinitions.forEach((arrow) => {
+    renderArrow(arrowGroup, geometry, arrow);
+  });
+
+  return svg;
+};
--- src/cube/parsing/algorithm.ts
+++ src/cube/parsing/algorithm.ts
@@ -9,13 +9,79 @@ export interface Turn {
   slices: number;
 }
 
-const turnRegex = /([2-9]+)?([UuFfRrDdLlBbMESxyz])(w)?(\d+\'|\'\d+|\d+|\')?/g;
+const turnRegex = /([2-9]+)?([UuFfRrDdLlBbMESxyz])(w)?(\d+'|'\d+|\d+|')?/g;
 
 const Opposite = {
   [TurnType.Clockwise]: TurnType.CounterClockwise,
   [TurnType.CounterClockwise]: TurnType.Clockwise,
   [TurnType.Double]: TurnType.Double,
 };
+
+function getSlices(rawSlices, outerBlockIndicator): number {
+  if (outerBlockIndicator && !rawSlices) {
+    return 2;
+  } if (!outerBlockIndicator && rawSlices) {
+    throw new Error(
+      'Invalid move: Cannot specify num slices if outer block move indicator \'w\' is not present',
+    );
+  } else if (!outerBlockIndicator && !rawSlices) {
+    return 1;
+  } else {
+    return parseInt(rawSlices, 10);
+  }
+}
+
+function getMove(rawFace: string): AlgorithmUnit {
+  if (possibleMoves.indexOf(rawFace) < 0) {
+    throw new Error(
+      `Invalid move (${rawFace}): Possible turn faces are [U R F L D B M E S x y z]`,
+    );
+  } else return rawFace as AlgorithmUnit;
+}
+
+function getTurnType(rawType: string): TurnType {
+  switch (rawType) {
+    case TurnAbbreviation.Clockwise:
+      return TurnType.Clockwise;
+    case TurnAbbreviation.CounterClockwise:
+      return TurnType.CounterClockwise;
+    case TurnAbbreviation.Double:
+    case TurnAbbreviation.DoubleCounter1:
+    case TurnAbbreviation.DoubleCounter2:
+      return TurnType.Double;
+    default:
+      // Attempt to parse non standard turn type
+      // (for invalid but reasonable moves like "y3")
+      let reversed = false;
+      if (rawType.charAt(0) === "'") {
+        reversed = true;
+        rawType = rawType.substring(1, rawType.length);
+      } else if (rawType.charAt(rawType.length - 1) === "'") {
+        reversed = true;
+      }
+
+      let turns = parseInt(rawType, 10) % 4;
+
+      if (Number.isNaN(turns)) {
+        throw new Error(`Invalid move modifier (${rawType})`);
+      }
+
+      if (turns === 0) {
+        return TurnType.None;
+      }
+
+      if (turns === 3) {
+        reversed = !reversed;
+        turns = 1;
+      }
+
+      if (turns === 2) {
+        return TurnType.Double;
+      }
+
+      return reversed ? TurnType.CounterClockwise : TurnType.Clockwise;
+  }
+}
 
 /**
  * Takes in an algorithm string and parses the turns from it
@@ -24,7 +90,7 @@ const Opposite = {
  *
  * https://www.worldcubeassociation.org/regulations/#article-12-notation
  */
-export function parseAlgorithm(algorithm: string): Turn[] {
+ export function parseAlgorithm(algorithm: string): Turn[] {
   if (!algorithm) {
     return [];
   }
@@ -65,70 +131,4 @@ export function parseCase(algorithm: string): Turn[] {
       slices: turn.slices,
     })
     .reverse();
-}
-
-function getSlices(rawSlices, outerBlockIndicator): number {
-  if (outerBlockIndicator && !rawSlices) {
-    return 2;
-  } if (!outerBlockIndicator && rawSlices) {
-    throw new Error(
-      'Invalid move: Cannot specify num slices if outer block move indicator \'w\' is not present',
-    );
-  } else if (!outerBlockIndicator && !rawSlices) {
-    return 1;
-  } else {
-    return parseInt(rawSlices);
-  }
-}
-
-function getMove(rawFace: string): AlgorithmUnit {
-  if (possibleMoves.indexOf(rawFace) < 0) {
-    throw new Error(
-      `Invalid move (${rawFace}): Possible turn faces are [U R F L D B M E S x y z]`,
-    );
-  } else return rawFace as AlgorithmUnit;
-}
-
-function getTurnType(rawType: string): TurnType {
-  switch (rawType) {
-    case TurnAbbreviation.Clockwise:
-      return TurnType.Clockwise;
-    case TurnAbbreviation.CounterClockwise:
-      return TurnType.CounterClockwise;
-    case TurnAbbreviation.Double:
-    case TurnAbbreviation.DoubleCounter1:
-    case TurnAbbreviation.DoubleCounter2:
-      return TurnType.Double;
-    default:
-      // Attempt to parse non standard turn type
-      // (for invalid but reasonable moves like "y3")
-      let reversed = false;
-      if (rawType.charAt(0) === "'") {
-        reversed = true;
-        rawType = rawType.substring(1, rawType.length);
-      } else if (rawType.charAt(rawType.length - 1) === "'") {
-        reversed = true;
-      }
-
-      let turns = parseInt(rawType) % 4;
-
-      if (isNaN(turns)) {
-        throw new Error(`Invalid move modifier (${rawType})`);
-      }
-
-      if (turns === 0) {
-        return TurnType.None;
-      }
-
-      if (turns === 3) {
-        reversed = !reversed;
-        turns = 1;
-      }
-
-      if (turns == 2) {
-        return TurnType.Double;
-      }
-
-      return reversed ? TurnType.CounterClockwise : TurnType.Clockwise;
-  }
 }
